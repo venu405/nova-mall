c4f107d752ed530e52a74e37b381c1345835e4ca
venu405 <venu405@users.noreply.github.com>
2026-09-11 11:01:26 +0800

    feat: 首页分类导航接入真实商品分类，替换死链占位图标
    - 10 个点击仅提示'敬请期待'的占位入口改为 8 个真实分类
    - 对接商品搜索接口 categoryId 参数（华为/iPhone/小米/荣耀手机、生活电器、数码相机、口红美妆）
    - 图标本地化，去除失效外链图片依赖

--- nova-mall-vue3-app/src/views/Home.vue
+++ nova-mall-vue3-app/src/views/Home.vue
@@ -25,7 +25,7 @@
     <nav-bar />
     <swiper :list="state.swiperList"></swiper>
     <div class="category-list">
-      <div v-for="item in state.categoryList" v-bind:key="item.categoryId" @click="tips">
+      <div v-for="item in state.categoryList" v-bind:key="item.categoryId" @click="goToCategory(item)">
         <img :src="item.imgUrl">
         <span>{{item.name}}</span>
       </div>
@@ -82,7 +82,7 @@ import swiper from '@/components/Swiper.vue'
 import navBar from '@/components/NavBar.vue'
 import { getHome } from '@/service/home'
 import { getLocal } from '@/common/js/utils'
-import { showLoadingToast, closeToast, showToast } from 'vant'
+import { showLoadingToast, closeToast } from 'vant'
 import { useCartStore } from '@/stores/cart'
 const cart = useCartStore()
 const router = useRouter()
@@ -95,45 +95,37 @@ const state = reactive({
   recommends: [],
   categoryList: [
     {
-      name: 'NovaMall超市',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E8%B6%85%E5%B8%82%402x.png',
-      categoryId: 100001
+      name: '华为手机',
+      imgUrl: 'http://localhost:28019/goods-img/nav/huawei.png',
+      categoryId: 46
     }, {
-      name: 'NovaMall服饰',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E6%9C%8D%E9%A5%B0%402x.png',
-      categoryId: 100003
+      name: '苹果iPhone',
+      imgUrl: 'http://localhost:28019/goods-img/nav/iphone.png',
+      categoryId: 47
     }, {
-      name: '全球购',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%85%A8%E7%90%83%E8%B4%AD%402x.png',
-      categoryId: 100002
+      name: '小米手机',
+      imgUrl: 'http://localhost:28019/goods-img/nav/xiaomi.png',
+      categoryId: 51
     }, {
-      name: 'NovaMall生鲜',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E7%94%9F%E9%B2%9C%402x.png',
-      categoryId: 100004
+      name: '荣耀手机',
+      imgUrl: 'http://localhost:28019/goods-img/nav/honor.png',
+      categoryId: 45
     }, {
-      name: 'NovaMall到家',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%88%B0%E5%AE%B6%402x.png',
-      categoryId: 100005
+      name: '生活电器',
+      imgUrl: 'http://localhost:28019/goods-img/nav/appliance.png',
+      categoryId: 20
     }, {
-      name: '充值缴费',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%85%85%E5%80%BC%402x.png',
-      categoryId: 100006
+      name: '数码相机',
+      imgUrl: 'http://localhost:28019/goods-img/nav/camera.png',
+      categoryId: 37
     }, {
-      name: '9.9元拼',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/9.9%402x.png',
-      categoryId: 100007
+      name: '口红美妆',
+      imgUrl: 'http://localhost:28019/goods-img/nav/beauty.png',
+      categoryId: 86
     }, {
-      name: '领劵',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E9%A2%86%E5%88%B8%402x.png',
-      categoryId: 100008
-    }, {
-      name: '省钱',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E7%9C%81%E9%92%B1%402x.png',
-      categoryId: 100009
-    }, {
-      name: '全部',
-      imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%85%A8%E9%83%A8%402x.png',
-      categoryId: 100010
+      name: '全部分类',
+      imgUrl: 'http://localhost:28019/goods-img/nav/all.png',
+      categoryId: 0
     }
   ],
   loading: true
@@ -169,8 +161,12 @@ const goToDetail = (item) => {
   router.push({ path: `/product/${item.goodsId}` })
 }
 
-const tips = () => {
-  showToast('敬请期待');
+const goToCategory = (item) => {
+  if (item.categoryId) {
+    router.push({ path: '/product-list', query: { categoryId: item.categoryId } })
+  } else {
+    router.push({ path: '/category' })
+  }
 }
 </script>
 
